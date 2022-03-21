b4a1a4409f4f7c75b3af25f16e47d3df70c28664
Calypso Leonard <calypsorleonard@gmail.com>
2022-03-21 14:20:33 -0400

    commit tree 5a5e543702cd9b8bc9fc31f9577c6d7ae90a1310

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
-// create your App component here
+import react, {useEffect, useState} from 'react'
+
+function App(){
+    const [curCurURL, setCur] = useState('#')
+    const [loaded, setLoaded] = useState(false)
+    useEffect( () => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(dod => {
+            setCur(dod.message)
+            setLoaded(true)
+        })
+    }, []
+        )
+    return (
+        <div> 
+            {loaded ? null:<p> Loading...</p>}
+            <img src={curCurURL} alt='A Random Dog'></img>
+        </div>
+    )
+}
+
+export default App
